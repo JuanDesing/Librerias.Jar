5c54a5b610341e4c5d9b31325ad6f0e7a68d26d4
jk <azeliorb@gmail.com>
2020-03-23 17:11:16 -0500

    primer commit

---
+++ AnimatedSlidingPanel/src/slidepanel/StateListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package slidepanel;
+
+/**
+ *
+ * @author root
+ */
+public interface StateListener {
+   public void toggleState() ;
+   public void reset() ;
+
+}
